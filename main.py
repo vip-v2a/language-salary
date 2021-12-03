c34e6ba87fe44e0d73952147370a2b5dc0a77e48
vip-v2a <vip.v2a@gmail.com>
2021-12-04 01:39:11 +0500

    Calculate 20 vacancies statictics

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import time
 import requests
-
+from itertools import count
 
 def get_hhru_response(text, area_id="1", period=30,
                       professional_role_id="96"):
+    """Docstrings need
+
+    """
+
     hhru_url = "https://api.hh.ru/vacancies"
     params = {
         "professional_role": professional_role_id,
@@ -35,6 +38,18 @@ def predict_rub_salary(vacancy):
     return 0.5 * (from_salary + to_salary)
 
 
+def fetch_records():
+    for page in count():
+        page_response = requests.get(url, params={'page': page})
+        page_response.raise_for_status()
+        page_data = page_response.json()
+
+        if page >= page_data['pages_number']:
+            break
+
+        yield from page_data['page_records']
+
+
 def main():
 
     programming_languages = [
@@ -49,18 +64,28 @@ def main():
         "React",
         "C#"
     ]
-    language_salary = {}
+    salary_statistics = {}
 
-    for language in programming_languages[:1]:
+    for language in programming_languages:
+
+        salaries = []
+
         hhru_response = get_hhru_response(language)
-        founded_vacancies = hhru_response["found"]
         vacancies = hhru_response["items"]
+
         for vacancy in vacancies:
             salary = predict_rub_salary(vacancy)
+            if salary:
+                salaries.append(salary)
             print(salary)
-        # time.sleep(1)
 
-    print(language_salary)
+        salary_statistics[language] = {
+            "vacancies_found": hhru_response["found"],
+            "vacancies_processed": len(salaries),
+            "average_salary": int(sum(salaries)/len(salaries))
+        }
+
+    print(salary_statistics)
 
 
 if __name__ == '__main__':
